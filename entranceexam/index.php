159479759112dd61202d53c6f6136aa3ae868e25
Abubakar Idris <86338723+abubakaridris2018@users.noreply.github.com>
2024-01-13 21:17:56 +0100

    Add files via upload

---
+++ entranceexam/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh">
+<title>ENTRACE EXAM</title>
+<script language="javascript">
+    window.location.href = "pages/index.php"
+</script>
+</head>
+<body>
+</body>
+</html>
